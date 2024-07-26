26f380335524fea6e115dd722d6a557f44c5a75c
Meetsimar <90956242+Meetsimar@users.noreply.github.com>
2024-07-25 20:06:25 -0400

    add setup script for installing packages bts

---
+++ scenario/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check and install python3-venv if not already installed
+if ! dpkg -l | grep -q python3-venv; then
+    sudo apt update && sudo apt install -y python3-venv
+fi
+
+# Create and activate a virtual environment
+python3 -m venv myenv
+source myenv/bin/activate
+
+# Install necessary Python packages
+pip install wheel setuptools
